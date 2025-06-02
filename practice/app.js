d202648561553c5afdf152120f85f3467eb21d1b
Janan <02.95.janan@gmail.com>
2025-06-02 22:50:14 +0500

    Updated

---
+++ practice/app.js
@@ -0,0 +1,10 @@
+
+let word = 'welcome Back to vsCode'
+let result = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+console.log(result);
+
+let word2 = `''`.toLowerCase();
+console.log(word2)
+
+let rightNow = new Date()
+console.log(rightNow.getDay());
